2450d0c271df6f0275a4dca3ac28780ea941492f
Bidisha Das <officialbidisha1@gmail.com>
2024-05-22 05:52:43 +0530

    feat:Machine Coding 1: Created modal

--- Modal/index.js
+++ Modal/index.js
@@ -1,1 +1,20 @@
+let modal = document.getElementById('simpleModal');
+let openModalButton = document.getElementsByClassName('btn')[0];
+let closeBtn = document.getElementsByClassName('closeBtn')[0];
 
+openModalButton.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', clickOutside);
+function openModal(){
+  modal.style.display = 'block';
+}
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+function clickOutside(e){
+    console.log('e.', e.target);
+    if(e.target === modal){
+        modal.style.display = 'none';
+    }
+}
