48d1d1b720339be6067d7360c90bfbe5d0df21e8
Bidisha Das <bidishadas@Bidishas-MacBook-Air.local>
2024-06-10 03:13:42 +0530

    fix:Added autocomplete

--- chipwithautosuggest/index.js
+++ chipwithautosuggest/index.js
@@ -1,33 +1,242 @@
-const addChipForm = document.getElementsByClassName("add-chip")[0];
-addChipForm.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const nameInput = document.getElementById("nameInput");
-  const name = nameInput.value.trim();
-  if (name !== "") {
-    createChip(name);
+const textBox = document.querySelector(".textbox");
+textBox.addEventListener("input", handleElement);
+const suggestions = document.getElementsByClassName("suggestions")[0];
+const countryList = [
+  "Afghanistan",
+  "Albania",
+  "Algeria",
+  "Andorra",
+  "Angola",
+  "Antigua and Barbuda",
+  "Argentina",
+  "Armenia",
+  "Australia",
+  "Austria",
+  "Azerbaijan",
+  "Bahamas",
+  "Bahrain",
+  "Bangladesh",
+  "Barbados",
+  "Belarus",
+  "Belgium",
+  "Belize",
+  "Benin",
+  "Bhutan",
+  "Bolivia",
+  "Bosnia and Herzegovina",
+  "Botswana",
+  "Brazil",
+  "Brunei",
+  "Bulgaria",
+  "Burkina Faso",
+  "Burundi",
+  "Cote dIvoire",
+  "Cabo Verde",
+  "Cambodia",
+  "Cameroon",
+  "Canada",
+  "Central African Republic",
+  "Chad",
+  "Chile",
+  "China",
+  "Colombia",
+  "Comoros",
+  "Congo",
+  "Costa Rica",
+  "Croatia",
+  "Cuba",
+  "Cyprus",
+  "Czechia",
+  "Democratic Republic of the Congo",
+  "Denmark",
+  "Djibouti",
+  "Dominica",
+  "Dominican Republic",
+  "Ecuador",
+  "Egypt",
+  "El Salvador",
+  "Equatorial Guinea",
+  "Eritrea",
+  "Estonia",
+  "Eswatini",
+  "Ethiopia",
+  "Fiji",
+  "Finland",
+  "France",
+  "Gabon",
+  "Gambia",
+  "Georgia",
+  "Germany",
+  "Ghana",
+  "Greece",
+  "Grenada",
+  "Guatemala",
+  "Guinea",
+  "Guinea-Bissau",
+  "Guyana",
+  "Haiti",
+  "Holy See",
+  "Honduras",
+  "Hungary",
+  "Iceland",
+  "India",
+  "Indonesia",
+  "Iran",
+  "Iraq",
+  "Ireland",
+  "Israel",
+  "Italy",
+  "Jamaica",
+  "Japan",
+  "Jordan",
+  "Kazakhstan",
+  "Kenya",
+  "Kiribati",
+  "Kuwait",
+  "Kyrgyzstan",
+  "Laos",
+  "Latvia",
+  "Lebanon",
+  "Lesotho",
+  "Liberia",
+  "Libya",
+  "Liechtenstein",
+  "Lithuania",
+  "Luxembourg",
+  "Madagascar",
+  "Malawi",
+  "Malaysia",
+  "Maldives",
+  "Mali",
+  "Malta",
+  "Marshall Islands",
+  "Mauritania",
+  "Mauritius",
+  "Mexico",
+  "Micronesia",
+  "Moldova",
+  "Monaco",
+  "Mongolia",
+  "Montenegro",
+  "Morocco",
+  "Mozambique",
+  "Myanmar",
+  "Namibia",
+  "Nauru",
+  "Nepal",
+  "Netherlands",
+  "New Zealand",
+  "Nicaragua",
+  "Niger",
+  "Nigeria",
+  "North Korea",
+  "North Macedonia",
+  "Norway",
+  "Oman",
+  "Pakistan",
+  "Palau",
+  "Palestine State",
+  "Panama",
+  "Papua New Guinea",
+  "Paraguay",
+  "Peru",
+  "Philippines",
+  "Poland",
+  "Portugal",
+  "Qatar",
+  "Romania",
+  "Russia",
+  "Rwanda",
+  "Saint Kitts and Nevis",
+  "Saint Lucia",
+  "Saint Vincent",
+  "Samoa",
+  "San Marino",
+  "Sao Tome and Principe",
+  "Saudi Arabia",
+  "Senegal",
+  "Serbia",
+  "Seychelles",
+  "Sierra Leone",
+  "Singapore",
+  "Slovakia",
+  "Slovenia",
+  "Solomon Islands",
+  "Somalia",
+  "South Africa",
+  "South Korea",
+  "South Sudan",
+  "Spain",
+  "Sri Lanka",
+  "Sudan",
+  "Suriname",
+  "Sweden",
+  "Switzerland",
+  "Syria",
+  "Tajikistan",
+  "Tanzania",
+  "Thailand",
+  "Timor-Leste",
+  "Togo",
+  "Tonga",
+  "Trinidad and Tobago",
+  "Tunisia",
+  "Turkey",
+  "Turkmenistan",
+  "Tuvalu",
+  "Uganda",
+  "Ukraine",
+  "United Arab Emirates",
+  "United Kingdom",
+  "United States of America",
+  "Uruguay",
+  "Uzbekistan",
+  "Vanuatu",
+  "Venezuela",
+  "Vietnam",
+  "Yemen",
+  "Zambia",
+  "Zimbabwe",
+];
 
-    // Use null if profile icon is empty
-    nameInput.value = "";
-  }
+textBox.addEventListener("blur", () => {
+  setTimeout(resetSuggestions, 200);
 });
 
-function createChip(name) {
-  const chipContainer = document.querySelector(".chips-container");
-  const chip = document.createElement("div");
-  chip.classList.add("chip");
-  const chipAvatar = document.createElement("div");
-  chipAvatar.classList.add("chip-avatar");
-  chipAvatar.textContent = name.substring(0, 1);
-  const chipName = document.createElement("div");
-  chipName.textContent = name;
-  const closeIcon = document.createElement("div");
-  closeIcon.classList.add("close-icon");
-  closeIcon.textContent = "x";
-  closeIcon.addEventListener("click", function () {
-    chip.remove();
-  });
-  chip.appendChild(chipAvatar);
-  chip.appendChild(chipName);
-  chip.appendChild(closeIcon);
-  chipContainer.appendChild(chip);
+suggestions.addEventListener('click', handleSuggestionsSelection)
+
+function handleSuggestionsSelection(event){
+    const element = event.target;
+    const chipsContainer = document.getElementsByClassName('chips-container')[0];
+    console.log(element);
+    const spanEle = document.createElement('span');
+    spanEle.className='chip';
+    spanEle.setAttribute('data-id', element.getAttribute('data-id'));
+    spanEle.textContent = element.textContent;
+    chipsContainer.appendChild(spanEle);
+    textBox.value = '';
+}
+
+function handleElement(event) {
+suggestions.innerHTML = "";
+  inputAdded = event.target.value;
+  if (inputAdded.trim().length > 0) {
+    let list = countryList.filter((country) => {
+      return (
+        country.toLowerCase().startsWith(inputAdded.toLowerCase())
+      );
+    });
+
+    list.forEach((element, index) => {
+      const elementList = document.createElement("li");
+      elementList.textContent = element;
+      elementList.setAttribute('data-id', index);
+      suggestions.appendChild(elementList);
+    });
+  }
+}
+
+function resetSuggestions() {
+  suggestions.innerHTML = "";
+  suggestionFocus = null;
 }
